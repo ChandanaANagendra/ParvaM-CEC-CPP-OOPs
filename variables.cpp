bfd46cf55fcaecd6f1959af04a8c4e52883bb417
ChandanaANagendra <chanduammuchandu3030@gmail.com>
2025-05-23 09:57:20 +0530

    C++ Basics & class concept

---
+++ variables.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //declaring the variables with different datatype
+    string name, college, branch, usn;
+    char section;
+    int age, sem;
+    bool isStudent;
+    float height;
+    //initializing/Assigning the values to the variables
+    name= "Chandana";
+    section='A';
+    age=18;
+    college="CEC";
+    branch="AI/ML";
+    usn="1CE24AI001";
+    sem=2;
+    isStudent=true;
+    height=5.2;
+
+    cout<< "Name: " << name << endl;
+    cout<< "section: " <<section << endl;
+    cout<< "age: " << age << endl;
+    cout<< "college: " << college << ", Branch: "<< branch <<  endl;
+    cout<< "usn: " << usn << " Sem: " << sem << endl;
+    cout<< "Section: " << section << " Is he/she a student: " << isStudent << endl;
+    //shift+Alt_bottom arrow - clone
+    //shift+Alt_bottom arrow - clone
+}
