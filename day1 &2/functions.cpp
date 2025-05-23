1135d264919c8f38a165f8e1aac9d43179966800
ChandanaANagendra <chanduammuchandu3030@gmail.com>
2025-05-23 16:32:14 +0530

    Access Specifiers, Scope Resolution Operator & Constructors

---
+++ day1 &2/functions.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void sayHello(){    //function declaration
+    cout << "Hello!" <<endl;    //function definition
+}
+
+int sayHi(string name){   // function with parameters
+    cout << "Hi, " << name <<endl;
+
+}
+
+int greet(string fname, string lname, int age){
+    cout << "Hi, " << fname << " " <<  lname << ", Age: "<< age << endl;
+}
+
+int main(){
+    string firstName,lastName, friendName;
+    int Age;
+    cout << "Enter your first name:";
+    cin >> firstName;
+    cout << "Enter your last name:";
+    cin >> lastName;
+    cout << "Enter your age:";
+    cin >> Age;
+    sayHello(); //function call
+    sayHi(firstName);    //function call with arguements
+    sayHi(friendName);
+    greet(firstName,lastName, Age);
+
+}
+  
